d68c2e5b38d5ba64ccd0bece986ba4fe25333620
Pranav Pahuja <20995301+pranavpahuja@users.noreply.github.com>
2021-03-22 15:42:18 +0530

    First commit

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Pranav Pahuja on 22/03/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
